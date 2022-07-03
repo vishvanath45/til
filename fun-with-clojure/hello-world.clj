4addf2edf7cbe232f5f9b7a320e0b51c7366eb8a
vishva.sharma <vishva.sharma@go-jek.com>
2022-07-03 23:33:35 +0530

    Take user input using read-line

--- fun-with-clojure/hello-world.clj
+++ fun-with-clojure/hello-world.clj
@@ -1,1 +1,14 @@
-(prn "Hello world")
+
+(defn hello-world
+([name]
+(prn (str "Hello " name)))
+([]
+(prn ("Hello World"))))
+ 
+(defn -main
+[& args]
+(let [name (read-line)]
+(hello-world name)))
+
+
+(-main)
